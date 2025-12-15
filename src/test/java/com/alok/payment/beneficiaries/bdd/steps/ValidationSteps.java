47f68208b81ace32c8896ced3df472e514acf070
FlutterOTA Setup <flutterota@example.com>
2025-12-15 21:35:48 +0000

    Document TestContainers Docker API compatibility issue

--- src/test/java/com/alok/payment/beneficiaries/bdd/steps/ValidationSteps.java
+++ src/test/java/com/alok/payment/beneficiaries/bdd/steps/ValidationSteps.java
@@ -11,8 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
 
-import java.util.Map;
-
 /**
  * Step definitions for beneficiary validation scenarios.
  */
